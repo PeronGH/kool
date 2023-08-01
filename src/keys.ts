b10eb6693337df81ae24d9f0c7753f5e5ba2da52
peron <peron_ap@icloud.com>
2023-08-02 02:14:21 +0800

    rename `KvStorage` to `ValueStore`

--- src/keys.ts
+++ src/keys.ts
@@ -1,11 +1,11 @@
 import type { MaybePromise } from "./utils.ts";
 
-export type KvStorage<T> = {
+export type ValueStore<T> = {
   get: () => MaybePromise<T>;
   set: (value: T) => MaybePromise<void>;
 };
 
-export function cached<T>({ get, set }: KvStorage<T>): KvStorage<T> {
+export function cached<T>({ get, set }: ValueStore<T>): ValueStore<T> {
   let value: T | null = null;
 
   return {
